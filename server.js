2130956f3538ca362554af6bf97829543d5925b5
Muhammedshahir001 <muhammedshahir91@gmail.com>
2026-08-20 18:32:53 +0530

    update the error

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'http://localhost:5000',
+  'https://misfits-one.vercel.app',
   'https://misfits-c1xursb7e-muhammed-shahir-s-projects.vercel.app',
 ];
 
