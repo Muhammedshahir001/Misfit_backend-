3b34a463285cb79b04f5bf1b2437d84b01d4cdd6
Muhammedshahir001 <muhammedshahir91@gmail.com>
2026-08-20 13:02:31 +0530

    new cors updated

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'http://localhost:5000',
-  'https://misfits-c1xursb7e-muhammed-shahir-s-projects.vercel.app',
+  'https://misfits-one.vercel.app',
 ];
 
 app.use(express.json({ limit: '10mb' }));
